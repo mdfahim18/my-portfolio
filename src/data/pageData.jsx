68d88a949b69fae880c1dd9fbd3cf999fc9ca608
mdfahim18 <mahmudulaminfahim@gmail.com>
2024-01-22 17:03:42 +0600

    some react basic projects added

--- src/data/pageData.jsx
+++ src/data/pageData.jsx
@@ -8,6 +8,14 @@ import { FaWhatsapp, FaLinkedin, FaFacebook } from 'react-icons/fa';
 import p1 from '../assets/projectsImages/project1.png';
 import p2 from '../assets/projectsImages/project2.png';
 import p3 from '../assets/projectsImages/project3.png';
+import p4 from '../assets/projectsImages/project4.png';
+import p5 from '../assets/projectsImages/project5.png';
+import p6 from '../assets/projectsImages/project6.png';
+import p7 from '../assets/projectsImages/project7.png';
+import p8 from '../assets/projectsImages/project8.png';
+import p9 from '../assets/projectsImages/project9.png';
+import p10 from '../assets/projectsImages/project10.png';
+import p11 from '../assets/projectsImages/project11.png';
 
 export const aboutData = [
   {
@@ -60,6 +68,70 @@ export const projectsData = [
     desc: 'This is React grocery bud project. This website is make by React and CSS',
     img: p2,
     link: 'https://my-react-grocery-bud-project.netlify.app',
+  },
+  {
+    id: 4,
+    category: 'CSS',
+    icon: <FaCss3 />,
+    desc: 'This is CSS E-commerce project. This website is make by HTML and CSS',
+    img: p4,
+    link: 'https://fahim-express.netlify.app',
+  },
+  {
+    id: 5,
+    category: 'React',
+    icon: <FaReact />,
+    desc: 'This is React Country project. This website is make by React and CSS',
+    img: p5,
+    link: 'https://my-react-country-app.netlify.app',
+  },
+  {
+    id: 6,
+    category: 'React',
+    icon: <FaReact />,
+    desc: 'This is React Unsplash Image project. This website is make by React and CSS',
+    img: p6,
+    link: 'https://my-react-unsplash-image-project.netlify.app',
+  },
+  {
+    id: 7,
+    category: 'React',
+    icon: <FaReact />,
+    desc: 'This is React Menu project. This website is make by React and CSS',
+    img: p7,
+    link: 'https://my-react-menu-project.netlify.app',
+  },
+  {
+    id: 8,
+    category: 'React',
+    icon: <FaReact />,
+    desc: 'This is React Navbar project. This website is make by React and CSS',
+    img: p8,
+    link: 'https://my-react-navbar-project.netlify.app',
+  },
+  {
+    id: 9,
+    category: 'React',
+    icon: <FaReact />,
+    desc: 'This is React Cocktail project. This website is make by React and CSS',
+    img: p9,
+    link: 'https://my-react-cocktail-project.netlify.app',
+  },
+  {
+    id: 10,
+    category: 'React',
+    icon: <FaReact />,
+    desc: 'This is React Cart project. This website is make by React and CSS',
+    img: p10,
+    link: 'https://my-react-cart-project1.netlify.app',
+  },
+  {
+    id: 11,
+    category: 'React',
+    icon: <FaReact />,
+    desc: 'This is React Submenu project. This website is make by React and CSS',
+    img: p11,
+    link: 'https://my-react-submenu.netlify.app',
   },
 ];
 
